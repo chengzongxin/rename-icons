d8c7270f0e442f35ac42be34a5cff3bdb7264dd1
chengzongxin <8876142@qq.com>
2024-11-05 22:43:48 +0800

    Clean up tracked files

---
+++ config.py
@@ -0,0 +1,5 @@
+# DeepBricks AI configuration
+DEEPBRICKS_CONFIG = {
+    'API_KEY': 'sk-PNab5CAdTnzN4ekzLMrELcAfp2fqCsaI07OookD41rJo2sSG',
+    'API_URL': 'https://api.deepbricks.ai/v1/chat/completions'
+} 
